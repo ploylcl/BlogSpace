ea238d59d384fa898a0431ba6eaa1be5b06f1028
AliceLs025 <kokonut280741@gmail.com>
2023-09-06 13:02:16 +0700

    add POST

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     }
     document.getElementById("blog-list").innerHTML =html
 })
+
+document.getElementById("new-post").addEventListener("submit", function(e){
+        e.preventDefault() //stop form from submitting
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    console.log(`${postTitle} ${postBody}`)
+    const data = {
+        title: postTitle,
+        body: postBody
+
+    }
+
+        const options ={
+        method:"POST",
+        body: JSON.stringify(data),
+        header:{
+            "content-type":"application.jason"
+        }
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts" , options)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+    })
+})
